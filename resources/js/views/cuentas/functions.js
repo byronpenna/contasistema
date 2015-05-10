231a2701f65e0c462bc370d6edaa7c560026080c
Byron Peña <byronpenna@gmail.com>
2015-05-10 00:07:15 -0600

    finalizacion de jornada

--- resources/js/views/cuentas/functions.js
+++ resources/js/views/cuentas/functions.js
@@ -2,12 +2,24 @@
 	function btnDelete(tr){
 		frm = serializeSection(tr);
 		console.log("formulario a eliminar",frm);
-		cargarObjetoGeneral(baseurl+"cuentas/ajax_agregarCuenta",frm,function(data){
-
+		cargarObjetoGeneral(baseurl+"cuentas/ajax_eliminarCuenta",frm,function(data){
+			console.log("respuesta del servidor(ajax_eliminarCuenta) ",data);
+			data = jQuery.parseJSON(data);
+			if(data.estado){
+				tr.remove();
+			}else{
+				alert("Ocurrio un error");
+			}
 		});
 	}
 	function guardarCuenta(frm){
 		cargarObjetoGeneral(baseurl+"cuentas/ajax_agregarCuenta",frm,function(data){
-			console.log("la respuesta del servidor es",data)
+			console.log("la respuesta del servidor es",data);
+			data = jQuery.parseJSON(data);
+			if(data.estado){
+				window.location.reload();
+			}else{
+				alert("Ocurrio un error");
+			}
 		});
 	}
