0a4f0e8b547ee376062251c046626a7a8b13ebe3
Byron Peña <byronpenna@gmail.com>
2015-05-12 22:17:40 -0600

    bla bla

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -5,11 +5,15 @@
 	<?php 
 		$this->load->view("parts/loads.php");
 	?>
-	<script type="text/javascript" src=<?php echo base_url("resources/js/views/welcome/script.js") ?> ></script>
-	<script type="text/javascript" src=<?php echo base_url("resources/js/views/welcome/functions.js") ?> ></script>
+	<!-- scripts -->
+		<script type="text/javascript" src=<?php echo base_url("resources/js/views/welcome/script.js") ?> ></script>
+		<script type="text/javascript" src=<?php echo base_url("resources/js/views/welcome/functions.js") ?> ></script>
+	<!-- styles -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/views/index/style.css") ?> >
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/views/index/media.css") ?> >
 </head>
 <body>
-	<div class="container">
+	<!-- <div class="container">
 		<h2>Login</h2>
 		<form id='frm'>
 			<div class="row">
@@ -39,7 +43,21 @@
 				</div>
 			</div>
 		</form>	
+	</div> -->
+	<div class = "container">
+	<div class="wrapper">
+		<form id="frm" class="form-signin">       
+		    <h3 class="form-signin-heading">Sistema contable IBPSV</h3>
+			<hr class="colorgraph"><br>
+			<input type="text" class="form-control" name="txtUsuario" placeholder="Username" required="" autofocus="" />
+			<input type="password" class="form-control" name="txtPass" placeholder="Password" required=""/>     		  
+			<button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>  			
+			<div class="row" id="divErrorMessage"> 
+				
+			</div>
+		</form>			
 	</div>
 	
+</div>
 </body>
 </html>
